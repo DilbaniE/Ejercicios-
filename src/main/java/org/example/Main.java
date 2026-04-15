c17e62f05076fc3d0ff095f0f3f0d8067ac161af
DilbaniE <claudiaenriquez241@gmail.com>
2026-04-14 21:59:30 -0500

    One

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizz");
+            }else if (i % 3 == 0) {
+                System.out.println("buzz");
+            }else if (i % 5 == 0) {
+                System.out.println("fizz buzz");
+            }else{
+                System.out.println("fizz buzz");
+            }}
+            
+    }
+}
