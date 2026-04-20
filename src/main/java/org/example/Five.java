fd0180ccef11de8d3613391c73acd78de2ea8955
DilbaniE <claudiaenriquez241@gmail.com>
2026-04-19 21:02:36 -0500

    Five

---
+++ src/main/java/org/example/Five.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Five {
+    public static void main(String[] args) {
+        int intentos = 1;
+        while (intentos <= 7) {
+            System.out.println("Numero " + intentos);
+            intentos++;
+        }
+    }
+}
