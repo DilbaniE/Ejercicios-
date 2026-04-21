6d187e448c6ac9cbec998a06633a0615d1ebef57
DilbaniE <claudiaenriquez241@gmail.com>
2026-04-20 19:53:54 -0500

    six

---
+++ src/main/java/org/example/Six.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Six {
+    public static void main(String[] args) {
+        int count = 5;
+        do {
+            System.out.println( "Numero " + (count--));
+        }while (count >= 1);
+    }
+}
