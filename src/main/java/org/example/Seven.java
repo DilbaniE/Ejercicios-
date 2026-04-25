85e8500e44b58d17b82e25966df8c904f166bab4
DilbaniE <claudiaenriquez241@gmail.com>
2026-04-25 10:00:30 -0500

    seven vs 2

--- src/main/java/org/example/Seven.java
+++ src/main/java/org/example/Seven.java
@@ -3,17 +3,61 @@ package org.example;
 public class Seven {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int edad = 10;
-        System.out.println(mayorDeEdad(edad));
+        int edad = 18;
+        int edad2 = 65;
+        int edad3 = 10;
+        int edad4 = 15;
+        int edad5 = 25;
+
+
+        System.out.println("Es mayor de edad: " + mayorDeEdad(edad));
+        System.out.println("Clasificacion de la edad "+  clasiEdad(edad));
+        System.out.println("Años por jubilarse " + anosJubilacion(edad));
+        System.out.println("----------");
+        System.out.println("Es mayor de edad: " + mayorDeEdad(edad2));
+        System.out.println("Clasificacion de la edad "+  clasiEdad(edad2));
+        System.out.println("Años por jubilarse " + anosJubilacion(edad2));
+        System.out.println("----------");
+        System.out.println("Es mayor de edad: " + mayorDeEdad(edad3));
+        System.out.println("Clasificacion de la edad "+  clasiEdad(edad3));
+        System.out.println("Años por jubilarse " + anosJubilacion(edad3));
+        System.out.println("----------");
+        System.out.println("Es mayor de edad: " + mayorDeEdad(edad4));
+        System.out.println("Clasificacion de la edad "+  clasiEdad(edad4));
+        System.out.println("Años por jubilarse " + anosJubilacion(edad4));
+        System.out.println("----------");
+        System.out.println("Es mayor de edad: " + mayorDeEdad(edad5));
+        System.out.println("Clasificacion de la edad "+  clasiEdad(edad5));
+        System.out.println("Años por jubilarse " + anosJubilacion(edad5));
+        System.out.println("----------");
+
     }
 
-        static String mayorDeEdad (int edad){
+        static boolean mayorDeEdad (int edad){
             if(edad>=18){
-                return "MAYOR DE EDAD";
+                return true;
             }else{
-                return "Menor de edad";
+                return false;
             }
+        }
 
+        static String clasiEdad(int edad){
+            if( edad >= 5 && edad <= 10){
+                return "niño";
+            }else if(edad>= 10 && edad < 18) {
+                return "Adolecente";
+            }else{
+                return "Adulto";
+            }
+        }
+
+        static  int anosJubilacion(int edad){
+          int jubilacion= 65;
+          int anosPorJubi = jubilacion - edad;
+            if (edad >= jubilacion) {
+                return 0;
+            }
+            return anosPorJubi ;
         }
 
 }
