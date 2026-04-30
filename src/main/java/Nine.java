3840a640a50ae5bf04e16667847cee8f5f3ebc2c
DilbaniE <claudiaenriquez241@gmail.com>
2026-04-30 18:07:14 -0500

    nine two

--- src/main/java/Nine.java
+++ src/main/java/Nine.java
@@ -1,8 +1,15 @@
 public class Nine {
+    public static void main(String[] args) {
+        System.out.println(potencia(2,10));
+        System.out.println(potencia(5,3));
 
+    }
     static int potencia(int base, int exp) {
-        for (int i = 1; i <= exp; i++) {
-
-        }
+        int count = 1;
+        System.out.println( base + " ^ " + exp + " = " );
+       for (int i = 0; i <= exp; i++) {
+           count = count * base;
+       }
+        return count;
     }
 }
