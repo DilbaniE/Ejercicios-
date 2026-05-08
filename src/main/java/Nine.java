ad6936f3cffcf689ab172c22bb8dc8e864d14f4f
DilbaniE <claudiaenriquez241@gmail.com>
2026-05-08 15:31:05 -0500

    three method

--- src/main/java/Nine.java
+++ src/main/java/Nine.java
@@ -2,7 +2,8 @@ public class Nine {
     public static void main(String[] args) {
         System.out.println(potencia(2,10));
         System.out.println(potencia(5,3));
-        System.out.println(factorial(5));
+        System.out.println(factorial(7));
+        System.out.println(numPrimo(17));
 
     }
     static int potencia(int base, int exp) {
@@ -13,17 +14,33 @@ public class Nine {
        }
         return count;
     }
+
     static int factorial(int n) {
-        int count = 1;
-        System.out.println(n + " ! " + " = ");
-        for (int i = 1; i <= n; i++) {
-            count = i * n;
-            count = count * i;
-            System.out.println(count);
-        }
         if (n == 0) {
             return 1;
         }
+        int count = 1;
+        System.out.println(n + " ! " + " = ");
+        for (int i = 1; i <= n; i++) {
+            count = count * i;
+        }
         return count;
     }
-}
+
+
+    static boolean numPrimo(int n) {
+        int result = 0;
+        System.out.println(n + " Es primo? ");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result = result + 1;
+            }
+        }
+        if (result == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    }
+
